8ed18c2cf4ef5974d43686d4914d5ac6ed1c186f
Boris Janevski <Boris.Janevski@netcetera.com>
2022-09-29 23:56:15 +0200

    [add-password-validation-tests] adding password validation tests

--- app/src/test/java/com/boryans/validate/ValidationFormTest.kt
+++ app/src/test/java/com/boryans/validate/ValidationFormTest.kt
@@ -14,4 +14,14 @@ class ValidationFormTest {
     //Then
     assertTrue(isValidated)
   }
+
+  @Test
+  fun `return true if password validation without mixed case value set`() {
+    //Given
+    val password = "menge#2!utebershenge"
+    //When
+    val isValidated = password.isValidPassword(shouldIncludeMixedCase = false)
+    //Then
+    assertTrue(isValidated)
+  }
 }
