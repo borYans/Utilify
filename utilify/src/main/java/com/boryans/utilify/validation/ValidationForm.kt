5102b41753d8d47e74a2e64411bbbfa312163673
Boris Janevski <Boris.Janevski@netcetera.com>
2022-10-01 23:02:24 +0200

    [added-utility-methods] created library

--- utilify/src/main/java/com/boryans/utilify/validation/ValidationForm.kt
+++ utilify/src/main/java/com/boryans/utilify/validation/ValidationForm.kt
@@ -8,13 +8,13 @@ import com.boryans.utilify.exception.NothingToValidateException
 fun String?.isValidEmail(): Boolean {
   this ?: throw NothingToValidateException("Validation is not possible on null value")
   if (isBlank()) return false
-  return this.trim().let { Regex(EMAIL_REGEX).matches(it) }
+  return this.trim().let { Regex(com.boryans.utilify.constants.EMAIL_REGEX).matches(it) }
 }
 
 fun String?.isValidCity(): Boolean {
   this ?: throw NothingToValidateException("Validation is not possible on null value")
   if (isBlank()) return false
-  return this.trim().let { Regex(CITY_REGEX).matches(it) }
+  return this.trim().let { Regex(com.boryans.utilify.constants.CITY_REGEX).matches(it) }
 }
 
 fun String?.isValidAge(
@@ -25,10 +25,4 @@ fun String?.isValidAge(
   if(isBlank()) return false
   val age = this.trim().toInt()
   return age in from..to
-}
-
-
-
-fun main() {
-
 }
